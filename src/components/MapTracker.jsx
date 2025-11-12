cbce6b9f835e072397c9cc86bc48e39a77758317
Akash Singh <akashgurjar6266@gmail.com>
2025-11-12 22:43:35 +0530

    Add Map tracker according to longitude and latitude

---
+++ src/components/MapTracker.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import L from "leaflet";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+export default function MapTracker({ positions }) {
+  // positions: array of [lat, lng]
+  const center = positions && positions.length ? positions[positions.length -1] : [26.2183,78.1828];
+  return (
+    <div style={{ height: '400px', width: '100%' }}>
+      <MapContainer center={center} zoom={15} style={{ height: '100%', width: '100%' }}>
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {positions && positions.length > 0 && (
+          <>
+            <Marker position={positions[positions.length -1]}>
+              <Popup>Live</Popup>
+            </Marker>
+            <Polyline positions={positions} />
+          </>
+        )}
+      </MapContainer>
+    </div>
+  );
+}
