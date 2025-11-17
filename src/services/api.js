4669462724329f3f0698aef67647727d9324443b
Akash Singh <akashgurjar6266@gmail.com>
2025-11-17 22:46:27 +0530

    Change the service api class to connect with backend because some configuration error

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,16 @@
-// services/api.js (example)
-import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8080/api",
+});
+
+// This function will add token automatically in axios header
+export const setAuthToken = (token) => {
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
+
 export default api;
