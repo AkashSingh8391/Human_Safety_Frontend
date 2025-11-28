dfa21fe6f2f3cba61023c7224da4e33cc08629f7
Akash Singh <akashgurjar6266@gmail.com>
2025-11-28 14:19:07 +0530

    Delete unnecessary file

--- src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const ProtectedRoute = ({ user, role, children }) => {
-  if (!user) return <Navigate to="/login" />;
-  if (role && user.role !== role) return <Navigate to="/login" />;
-  return children;
-};
