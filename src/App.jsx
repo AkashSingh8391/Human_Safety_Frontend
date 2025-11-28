f0f897869e00dfc3da60a3d79f584da5143e201c
Akash Singh <akashgurjar6266@gmail.com>
2025-11-28 14:09:21 +0530

    Change App.jsx because no need to mut=ltiple dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import CivilDashboard from "./pages/CivilDashboard";
-import PoliceDashboard from "./pages/PoliceDashboard";
-
 
 function App() {
   return (
@@ -11,7 +9,6 @@ function App() {
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/civil" element={<CivilDashboard />} />
-      <Route path="/police" element={<PoliceDashboard />} />
     </Routes>
   );
 }
