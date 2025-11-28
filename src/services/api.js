05d7747125ed4b6ec1b26589a032f14236773b05
Akash Singh <akashgurjar6266@gmail.com>
2025-11-28 14:12:38 +0530

    Connect with backend

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,10 @@ const api = axios.create({
   baseURL: "http://localhost:8080/api",
 });
 
-// This function will add token automatically in axios header
-export const setAuthToken = (token) => {
-  if (token) {
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  } else {
-    delete api.defaults.headers.common["Authorization"];
-  }
-};
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 export default api;
