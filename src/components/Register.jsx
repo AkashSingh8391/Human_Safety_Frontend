cae70f361d4d140e0bd55ce094ad4d1461e46d75
Akash Singh <akashgurjar6266@gmail.com>
2025-12-03 17:35:55 +0530

    Create new Register page

---
+++ src/components/Register.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  async function register(e) {
+    e.preventDefault();
+    try {
+      await api.post("/auth/register", { email, password });
+      alert("Registered! Please login.");
+      navigate("/login");
+    } catch {
+      alert("Registration Failed");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h2>Register</h2>
+
+      <form onSubmit={register}>
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+
+        <button type="submit">Register</button>
+      </form>
+
+      <p onClick={() => navigate("/login")}>Already Have Account?</p>
+    </div>
+  );
+}
